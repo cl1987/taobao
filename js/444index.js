a628b09b29a4d0d06f13a118d47630c88536469e
cl1987 <2630636922@qq.com>
2019-07-14 23:08:24 +0800

    121

--- js/444index.js
+++ js/444index.js
@@ -73,23 +73,6 @@
 	//加载数据
 	$topdropdown.on('dropdown-show dropdown-shown dropdown-hide dropdown-hidden',function(ev){
 		if(ev.type=='dropdown-show'){
-			/*
-			var url=$elem.data('load')
-			// console.log(url);
-			//如果没有地址，则无需加载数据
-			if(!url) return; 
-			//如果第一次进入则加载数据,后面再进入则不需要
-			if(!$elem.data('isloaded')){
-				$.getJSON(url,function(data){
-					console.log(data)
-					//生成HTML
-					var html='';
-					for(var i=0;i<data.length;i++){
-						html+='<li><a href="'+data[i].url+'">'+data[i].name+'</a></li>'
-					}
-					
-			})
-			*/
 			loadHtmlOnce($(this),buildTopLayer)
 		}
 	})
@@ -108,22 +91,7 @@
 			$elem.data('isloaded',true)
 		},1000)	
 	}
-	
-	
-		// 原生js方法
-	/*
-		var $dropdown=$('.dropdown');
-		$dropdown
-		.hover(function(){
-			// console.log($(this).data('active'));
-			var activeClass=$(this).data('active')+"-active"
-			$(this).addClass(activeClass);
-		},function(){
-			var activeClass=$(this).data('active')+'active'
-			$(this).removeClass(activeClass);
-		})
-	*/
-	// 顶部导航逻辑结束
+// 顶部导航逻辑结束..........................
 	// $('button').on('click',function(){
 	// 	$dropdown.dropdown('show')
 	// })
@@ -204,46 +172,7 @@
 
 
 //轮播图懒加载共通开始.....................
-/*
-	function courselLazyLoad($elem){
-		var item={};
-		var totalNum=$elem.find('.carousel-img').length-1
-		var totalLoadNum=0;
-		var totalFn=0
-		// 1.开始加载
-		$elem.on('coursel-show',totalFn=function(ev,index,elem){
-			if(!item[index]){
-				$elem.trigger('coursel-load',[index,elem])	
-			}
-		})
-		// 2.执行加载
-		$elem.on('coursel-load',function(ev,index,elem){
-			var $elem=$(elem);
-			var $imgs=$elem.find('.carousel-img')
-			$imgs.each(function(){
-				var $img = $(this);
-				var imgUrl=$img.data('src')
-				loadImg(imgUrl,function(){
-					$img.attr('src',imgUrl)
-				},function(){
-					$img.attr('src','images/focus-carousel/placeholder.png')
-				})
-			})
-			//图片已经被加载
-			item[index]='isloaded'
-			totalLoadNum++
-			//所有图片都被加载则移除事件
-			if(totalLoadNum>totalNum){
-				$elem.trigger('coursel-loaded')
-			}
-		})
-		// 3.加载完毕
-		$elem.on('coursel-loaded',function(){
-			$elem.off('coursel-show',totalFn)
-		})	
-	}	
 
-*/
 
 //轮播图懒加载共通结束.....................
 
@@ -251,41 +180,6 @@
 
 //轮播图逻辑开始.....................
 	var $coursel = $('.carousel .carousel-wrap');
-	/*
-		$coursel.item={};
-		$coursel.totalNum=$coursel.find('.carousel-img').length-1
-		$coursel.totalLoadNum=0;
-		$coursel.totalFn=0
-		// 1.开始加载
-		$coursel.on('coursel-show',$coursel.totalFn=function(ev,index,elem){
-			if(!$coursel.item[index]){
-				$coursel.trigger('coursel-load',[index,elem])	
-			}
-		})
-		// 2.执行加载
-		$coursel.on('coursel-load',function(ev,index,elem){
-			var $elem=$(elem);
-			var $img=$elem.find('.carousel-img')
-			var imgUrl=$img.data('src')
-			loadImg(imgUrl,function(){
-				$img.attr('src',imgUrl)
-			},function(){
-				$img.attr('src','images/focus-carousel/placeholder.png')
-			})
-			//图片已经被加载
-			$coursel.item[index]='isloaded'
-			$coursel.totalLoadNum++
-			//所有图片都被加载则移除事件
-			if($coursel.totalLoadNum>$coursel.totalNum){
-				$coursel.trigger('coursel-loaded')
-			}
-		})
-		// 3.加载完毕
-		$coursel.on('coursel-loaded',function(){
-			$coursel.off('coursel-show',$coursel.totalFn)
-		})
-	*/
-	// courselLazyLoad($coursel)
 	lazyLoad({
 		totalNum:$coursel.find('.carousel-img').length - 1,
 		$elem:$coursel,
@@ -294,7 +188,6 @@
 	})
 	//2.执行加载
 	$coursel.on('coursel-load',function(ev,index,elem,success){
-		// console.log('will load img',index);
 		var $elem = $(elem);
 		var $imgs = $elem.find('.carousel-img');
 		$imgs.each(function(){
@@ -313,44 +206,9 @@
 // 轮播图逻辑结束.....................
 
 //今日热销开始
-	var $todaysCoursel = $('.today .carousel .carousel-wrap');
-	/*
-		$todaysCoursel.item={};
-		$todaysCoursel.totalNum=$todaysCoursel.find('.carousel-img').length-1
-		$todaysCoursel.totalLoadNum=0;
-		$todaysCoursel.totalFn=0
-		// 1.开始加载
-		$todaysCoursel.on('coursel-show',$todaysCoursel.totalFn=function(ev,index,elem){
-			if(!$todaysCoursel.item[index]){
-				$todaysCoursel.trigger('coursel-load',[index,elem])	
-			}
-		})
-		// 2.执行加载
-		$todaysCoursel.on('coursel-load',function(ev,index,elem){
-			var $elem=$(elem);
-			var $imgs=$elem.find('.carousel-img')
-			$imgs.each(function(){
-				var imgUrl=$img.data('src')
-				loadImg(imgUrl,function(){
-					$img.attr('src',imgUrl)
-				},function(){
-					$img.attr('src','images/focus-carousel/placeholder.png')
-				})
-			})
-			//图片已经被加载
-			$todaysCoursel.item[index]='isloaded'
-			$todaysCoursel.totalLoadNum++
-			//所有图片都被加载则移除事件
-			if($todaysCoursel.totalLoadNum>$todaysCoursel.totalNum){
-				$todaysCoursel.trigger('coursel-loaded')
-			}
-		})
-		// 3.加载完毕
-		$todaysCoursel.on('coursel-loaded',function(){
-			$todaysCoursel.off('coursel-show',$todaysCoursel.totalFn)
-		})
-	*/
-	// courselLazyLoad($todaysCoursel)
+/*
+	var $todaysCoursel = $('.today .carousel-wrap');
+	
 	lazyLoad({
 		totalNum:$todaysCoursel.find('.carousel-img').length - 1,
 		$elem:$todaysCoursel,
@@ -375,49 +233,37 @@
 		})
 	})
 	$todaysCoursel.coursel({});
+*/
+
+		var $todaysCoursel = $('.todays .carousel-wrap');
+		lazyLoad({
+			totalNum:$todaysCoursel.find('.carousel-img').length - 1,
+			$elem:$todaysCoursel,
+			eventName:'coursel-show',
+			eventPrifix:'coursel'
+		})
+		//2.执行加载
+		$todaysCoursel.on('coursel-load',function(ev,index,elem,success){
+			var $elem = $(elem);
+			var $imgs = $elem.find('.carousel-img');
+			$imgs.each(function(){
+				var $img = $(this);
+				var imgUrl = $img.data('src');
+				loadImg(imgUrl,function(){
+					$img.attr('src',imgUrl);
+				},function(){
+					$img.attr('src','images/focus-carousel/placeholder.png');
+				});
+				//加载完成后执行函数
+				success();
+			})
+		})
+		$todaysCoursel.coursel({});
+
 //今日热销结束............
 
 //楼层逻辑开始............
 //楼层图片懒加载
-/*
-function floorImgLazyLoad($elem){
-	var item={};
-	var totalNum=$elem.find('.floor-img').length-1
-	var totalLoadNum=0;
-	var totalFn=0
-	// 1.开始加载
-	$elem.on('tab-show',totalFn=function(ev,index,elem){
-		if(!item[index]){
-			$elem.trigger('tab-load',[index,elem])	
-		}
-	})
-	// 2.执行加载
-	$elem.on('tab-load',function(ev,index,elem){
-		var $elem=$(elem);
-		var $imgs=$elem.find('.floor-img')
-		$imgs.each(function(){
-			var $img = $(this);
-			var imgUrl=$img.data('src')
-			loadImg(imgUrl,function(){
-				$img.attr('src',imgUrl)
-			},function(){
-				$img.attr('src','images/focus-carousel/placeholder.png')
-			})
-		})
-		//图片已经被加载
-		item[index]='isloaded'
-		totalLoadNum++
-		//所有图片都被加载则移除事件
-		if(totalLoadNum>totalNum){
-			$elem.trigger('tab-loaded')
-		}
-	})
-	// 3.加载完毕
-	$elem.on('tab-loaded',function(){
-		$elem.off('tab-show',totalFn)
-	})	
-}
-*/
 //生成楼层html
 function buildFloorHtml(oneFloorData){
 	var html=''
@@ -471,51 +317,10 @@ function buildFloorBodyHtml(oneFloorData){
 		return html;
 }
 //楼层html懒加载
-/*
-function floorHtmlLazyLoad(){
-	var item={};
-	var totalNum=$floor.length-1
-	var totalLoadNum=0;
-	var totalFn=0
-	// 1.开始加载
-	$doc.on('floor-show',totalFn=function(ev,index,elem){
-		if(!item[index]){
-			$doc.trigger('floor-load',[index,elem])	
-		}
-	})
-	// 2.执行加载
-	$doc.on('floor-load',function(ev,index,elem){
-		getDataOnce($doc,'data/floor/floorData.json',function(data){
-			// console.log(data[index])
-			//1生成html结构
-			var html=buildFloorHtml(data[index]);
-			//2加载html结构
-			$(elem).html(html);
-			//3楼层图片懒加载
-			floorImgLazyLoad($(elem));
-			//4.激活选项卡
-			$(elem).tab({});
-		})
-		//图片已经被加载
-		item[index]='isloaded'
-		totalLoadNum++
-		//所有图片都被加载则移除事件
-		if(totalLoadNum>totalNum){
-			$doc.trigger('floor-loaded')
-		}
-	})
-	// 3.加载完毕
-	$doc.on('floor-loaded',function(){
-		$doc.off('floor-show',totalFn)
-	})	
-}
-*/
 //判断是否在可视区
 function isVisible($elem){
 	return ($win.height() + $win.scrollTop() > $elem.offset().top) && ($elem.offset().top + $elem.height() > $win.scrollTop())
-}	
-// $win.scrollTop()+$win.height()>$elem.offset().top && $elem.height+$elem.offset().top>$win.scrollTop()
-//return ($win.height() + $win.scrollTop() > $elem.offset().top) && ($elem.offset().top + $elem.height() > $win.scrollTop())
+}
 	var $floor=$('.floor');
 	var $win=$(window)
 	var $doc=$(document)
@@ -563,18 +368,11 @@ function isVisible($elem){
 		});
 		//加载成功执行函数
 		success();
-		console.log(111)
+		// console.log(111)
 	})
-	// floorHtmlLazyLoad();
-	//遍历每一个楼层实现图片懒加载
-	/*
-		$floor.each(function(){
-			floorImgLazyLoad($(this));
-		})
-	*/
-	$doc.on('floor-show',function(ev,index,$elem){
-		console.log(index,$elem)
-	})
+	// $doc.on('floor-show',function(ev,index,$elem){
+	// 	console.log(index,$elem)
+	// })
 	function timeToShow(){
 		$floor.each(function(index,elem){
 			if(isVisible($(elem))){//这里为什么不是$elem呢？还要加括号
